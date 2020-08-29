0508be5ecfbabc2ad558cdfe631bb102d51e9dfd
qaztronic <qaztronic@gmail.com>
2020-08-29 13:33:06 -0700

    -

---
+++ sim/tests/tb_mnist_mlp/files.f
@@ -0,0 +1,17 @@
+#
+
+${LIB_BASE_DIR}/axi4_stream_lib/src/recursive_axis_catenate.sv
+
+${LIB_BASE_DIR}/basal/src/ROM/rom.sv
+${LIB_BASE_DIR}/basal/src/ROM/axis_rom.sv
+
+${PROJECT_DIR}/src/math/axis_mac.sv
+
+${PROJECT_DIR}/src/nn/relu.sv
+${PROJECT_DIR}/src/nn/linear.sv
+
+${PROJECT_DIR}/src/nn/axis_dense_neuron.sv
+${PROJECT_DIR}/src/nn/axis_dense.sv
+
+${PROJECT_DIR}/src/mnist_mlp/mnist_mlp_top.sv
+
